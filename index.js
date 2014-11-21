49fe9e728284174bc51268800bc3d25f6abb87a3
Kevin Martensson <kevinmartensson@gmail.com>
2014-11-21 12:04:38 +0100

    Beginning of line or white space is optional

--- index.js
+++ index.js
@@ -7,5 +7,5 @@
  */
 
 module.exports = function () {
-	return /(?:^|\s)(["\'])?(?:(?:callto|skype):)(?:[a-zA-Z][a-zA-Z0-9\.,\-_]{5,31})(?:\?(?:add|call|chat|sendfile|userinfo))?\1/ig;
+	return /(?:^|\s)?(["\'])?(?:(?:callto|skype):)(?:[a-zA-Z][a-zA-Z0-9\.,\-_]{5,31})(?:\?(?:add|call|chat|sendfile|userinfo))?\1/ig;
 };
