d19ce28bd41936cab01dfd1e74ee1e02e6eeaacf
Kevin Martensson <kevinmartensson@gmail.com>
2015-06-18 10:33:57 +0200

    Don't capture whitespace

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ module.exports = function (opts) {
 	var regex = '(?:(?:callto|skype):)(?:[a-zA-Z][a-zA-Z0-9\\.,\\-_]{5,31})(?:\\?(?:add|call|chat|sendfile|userinfo))?';
 
 	return opts.exact ? new RegExp('(?:^' + regex + '$)', 'i') :
-						new RegExp('(?:^|\\s)?(["\'])?' + regex + '\\1', 'ig');
+						new RegExp('(["\'])?' + regex + '\\1', 'ig');
 };
