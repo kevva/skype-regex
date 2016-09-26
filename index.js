dc975a02cb283bd41adc25ee3569887d855e9d6a
Kevin Martensson <kevinmartensson@gmail.com>
2016-09-26 14:21:21 +0200

    Use template literals

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 module.exports = opts => {
 	opts = opts || {};
 	const regex = '(?:(?:callto|skype):)(?:[a-z][a-z0-9\\.,\\-_]{5,31})(?:\\?(?:add|call|chat|sendfile|userinfo))?';
-	return opts.exact ? new RegExp('(?:^' + regex + '$)', 'i') : new RegExp('(["\'])?' + regex + '\\1', 'ig');
+	return opts.exact ? new RegExp(`(?:^${regex}$)`, 'i') : new RegExp(`(["\'])?${regex}\\1`, 'ig');
 };
